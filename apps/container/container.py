1faef8ba9bb4375f42b59fffcf9503321e488e4c
eddiekampe <eddiek@kth.se>
2014-12-30 23:34:08 +0100

    Added total number of containers and images in navigation

--- apps/container/container.py
+++ apps/container/container.py
@@ -1,16 +1,14 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from docker import Client
+from flask import Blueprint, render_template, redirect, url_for, flash, request, g
 
 container = Blueprint("container", __name__)
-docker_client = Client(base_url="unix://var/run/docker.sock")
 
 
 # List containers
 @container.route("/", methods=["GET"])
 def list_containers():
 
-    containers = docker_client.containers(all=True)
-    images = docker_client.images()
+    containers = g.docker_client.containers(all=True)
+    images = g.docker_client.images()
 
     return render_template("container/list.html", containers=containers, images=images)
 
@@ -18,14 +16,14 @@ def list_containers():
 # Show container by id
 @container.route("/<container_id>", methods=["GET"])
 def inspect_container(container_id):
-    entry = docker_client.inspect_container(container_id)
+    entry = g.docker_client.inspect_container(container_id)
     return render_template("container/entry.html", container=entry)
 
 
 # Delete container by id
 @container.route("/<container_id>/delete", methods=["GET"])
 def delete_container(container_id):
-    feedback = docker_client.remove_container(container_id)
+    feedback = g.docker_client.remove_container(container_id)
     print feedback
     return redirect(url_for("container.list_containers"))
 
@@ -33,7 +31,7 @@ def delete_container(container_id):
 # Start container by id
 @container.route("/<container_id>/start", methods=["GET"])
 def start_container(container_id):
-    feedback = docker_client.start(container_id)
+    feedback = g.docker_client.start(container_id)
     print feedback
     return redirect(url_for("container.list_containers"))
 
@@ -47,7 +45,7 @@ def create_container():
     image = request.form["image"]
 
     if None not in [image, container_name]:
-        new_container = docker_client.create_container(image=image, name=container_name)
+        new_container = g.docker_client.create_container(image=image, name=container_name)
         print new_container
 
     return redirect(url_for("container.list_containers"))
--- harbor.py
+++ harbor.py
@@ -1,7 +1,7 @@
 from docker import Client
 import os
 
-from flask import Flask, url_for, redirect, send_from_directory, json, render_template
+from flask import Flask, url_for, redirect, send_from_directory, json, render_template, g
 
 from apps.image.image import image
 from apps.container.container import container
@@ -12,7 +12,10 @@ application = app = Flask(__name__)
 app.register_blueprint(image, url_prefix="/images")
 app.register_blueprint(container, url_prefix="/containers")
 
-docker_client = Client(base_url="unix://var/run/docker.sock")
+
+@app.before_request
+def setup():
+    g.docker_client = Client(base_url="unix://var/run/docker.sock")
 
 
 @app.errorhandler(404)
@@ -27,10 +30,18 @@ def server_error(error):
     return error  # redirect(url_for("home"))
 
 
+@app.context_processor
+def prepare_request():
+    # Fetch number of images and containers
+    images = g.docker_client.images(quiet=True)
+    containers = g.docker_client.containers(quiet=True, all=True)
+    return dict(num_images=len(images), num_containers=len(containers))
+
+
 @app.route("/")
 def overview():
-    version = docker_client.version()
-    info = docker_client.info()
+    version = g.docker_client.version()
+    info = g.docker_client.info()
     return render_template("overview.html", version=version, info=info)
 
 
@@ -41,7 +52,7 @@ def about():
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'images/favicon.ico')
+    return send_from_directory(os.path.join(app.root_path, "static"), "images/favicon.ico")
 
 
 @app.route("/environment")
