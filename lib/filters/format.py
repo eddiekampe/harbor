2334962f018bdf1042180e4b5453dadfd39b8b3a
Eddie Kämpe <eddiek@kth.se>
2015-02-27 14:39:07 -0800

    Bug fix: Added missing file for: 232a2322cc15b6c41c26d149c33e2df9b629d4b5

--- lib/filters/format.py
+++ lib/filters/format.py
@@ -4,6 +4,7 @@ from datetime import datetime
 def register(app):
     app.jinja_env.filters["b_to_mb"] = b_to_mb
     app.jinja_env.filters["date_string"] = date_string
+    app.jinja_env.filters["log_text"] = log_text
 
 
 def b_to_mb(byte_string):
@@ -19,3 +20,20 @@ def date_string(timestamp):
     Example: 1414106559 -> 24-10-2014
     """
     return datetime.fromtimestamp(timestamp).strftime("%d-%m-%Y")
+
+
+def log_text(logs):
+    """
+    Clean docker logs into a readable format
+    :param logs: Log entries
+    :return: Cleaned version of the logs
+    """
+
+    # Unsure why this is in the log output
+    replacements = ("[32m", ""), \
+                   ("[36m", ""), \
+                   ("[38m", ""), \
+                   ("[39m", ""), \
+                   ("[90m", "")
+
+    return reduce(lambda in_string, kv: in_string.replace(*kv), replacements, logs)
